0a61b954ad5111c69471c0400e8ea4e2a5f7cb50
lucianoacosta <lucianoacosta338@gmail.com>
2026-01-28 14:03:13 -0300

    clase 2 - condicionales y ciclos

--- introduccion/clase2.js
+++ introduccion/clase2.js
@@ -1,5 +1,40 @@
-//let edadUsuario = Number(prompt("Por favor, ingresa tu edad:"));
-//console.log(edadUsuario >= 18 ? "si, podes pasar" : "no, no podes pasar");
-num1 = 2;
-num2 = 5;
-console.log(num1 + num2);
+//_________________________CONDICIONALES_________________________
+/*
+//if --> uso cuando tengo un solo camino, o cuando tengo mas de 2
+//ternario --> uso cuando tengo 2 caminos
+//switch --> uso cuando tengo mas de 2 caminos y cada camino es muy diferente a los demas
+let edadUsuario = Number(prompt("Por favor, ingresa tu edad:"));
+console.log(edadUsuario >= 18 ? "si, podes pasar" : "no, no podes pasar");
+/*
+
+//contorlar el accesp a un recital de musica
+//si es mayor lo dejo pasar
+//si es menor pero viene con un adulto lo dejo pasar, le mostramos que si puede pasar con el adulto
+//si es menor y no viene con un adulto no lo dejo pasar
+
+/*
+let edadPersona = 12;
+let vieneAcompaniado = true;
+
+if (edadPersona >= 18) {
+    console.log("Podes pasar al recital, sos mayor de edad");
+} else if(edadPersona < 18 && vieneAcompaniado) {
+    console.log("Podes pasar al recital, pero solo porque venis acompañado");
+    } else {
+        console.log("No podes pasar al recital, sos menor de edad y no venis acompañado");
+    }
+*/
+//_________________________CICLOS_________________________
+//while --> uso cuando no se conoci de antemano la cantidad de iteraciones
+
+//for --> uso cuando conozco de antemano la cantidad de iteraciones
+//inicializacion; condicion; modificador
+/*
+let num = 15;
+num ++; = 16
+num--;  = 14
+*/
+for (let i = 1; i <= 10; i++) {
+  console.log("hola", i);
+}
+console.log("fin del ciclo");
--- introduccion/variables.js
+++
@@ -1,9 +0,0 @@
-let nombre = prompt("ingresa tu nombre");
-
-const saludar = () => {
-    console.log("hola " + nombre);
-}
-saludar();
-
-let btn = document.getElementById("boton-Saludar");
-btn.addEventListener("click", saludar);
